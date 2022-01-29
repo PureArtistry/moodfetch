ec5d46588cbc3c72ab0b4af720b993f8cd6555c0
David Sherriff <d.sherriff.81@gmail.com>
2022-01-29 00:46:42 +0000

    Update update_fetch_pkgcount.sh
    added extra q to pacman commands to speed things up

--- pacman_hook/usr/local/bin/update_fetch_pkgcount.sh
+++ pacman_hook/usr/local/bin/update_fetch_pkgcount.sh
@@ -4,8 +4,8 @@
 # edit this to the absolute path to your cache folder
 cache_path=/home/dave/.cache
 
-n=$(pacman -Qn --color never | wc -l)
-m=$(pacman -Qm --color never | wc -l)
+n=$(pacman -Qqn --color never | wc -l)
+m=$(pacman -Qqm --color never | wc -l)
 [ -d ${cache_path}/moodfetch ] || mkdir -p ${cache_path}/moodfetch
 echo "${n} ${m}" > ${cache_path}/moodfetch/pkg_stats
 
